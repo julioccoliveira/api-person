a87f1ed0852aea6196e4533c338fff0b3463da96
Júlio Oliveira <juliocc_oliveira@hotmail.com>
2024-04-29 20:11:37 -0300

    Spring initalizr files

---
+++ src/main/java/com/github/julio/apiperson/ApiPersonApplication.java
@@ -0,0 +1,13 @@
+package com.github.julio.apiperson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPersonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiPersonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/julio/apiperson/ApiPersonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.julio.apiperson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPersonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
